c5445c6dac5b009ae7ca0ff390f0a2ccbedf4230
James Armenta <armentajames@gmail.com>
2016-01-18 12:17:39 -0800

    Escape Key

--- js/project.js
+++ js/project.js
@@ -2,31 +2,32 @@ $( document ).ready(function() {
 
  var timing = 800;
 
-$(document).keyup(function(e) {
-     if (e.keyCode == 27) { // escape key maps to keycode `27`
-        $("body").animate({opacity: 0}, timing/2);
-        window.location = getRootUrl();
-    }
-});
+ document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 27) {
+    $("body").animate({opacity: 0}, timing/2);
+    alert("Escape");
+  }
+};
 
 function getRootUrl() {
   return window.location.origin?window.location.origin+'/':window.location.protocol+'/'+window.location.host+'/';
 }
 
- $("a").click(function ( event ){
-   event.preventDefault();
-   $("body").animate({opacity: 0}, timing/2);
-   href = $(this).attr('href');
-   setTimeout(function() {window.location = href}, timing/2);
- });
+$("a").click(function ( event ){
+ event.preventDefault();
+ $("body").animate({opacity: 0}, timing/2);
+ href = $(this).attr('href');
+ setTimeout(function() {window.location = href}, timing/2);
+});
 
- $("body").animate({opacity: 1}, timing/2);
+$("body").animate({opacity: 1}, timing/2);
 
- var tabletWidth = 1000;
- var mobileWidth = 600;
- var freshpage = function() {
+var tabletWidth = 1000;
+var mobileWidth = 600;
+var freshpage = function() {
 
-    if($(window).width() > tabletWidth ){
+  if($(window).width() > tabletWidth ){
     //parallax movement onscroll
     var maxDistance = 60;
     $(".lax").each(function(){
